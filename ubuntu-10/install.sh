cba0878cb42aa6baa2a2efafb889d96e980a6e68
kitofr <kitofr@gmail.com>
2010-05-04 11:30:12 +0200

    added assume-yes and fixed passenger-conf file

--- ubuntu-10/install.sh
+++ ubuntu-10/install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # editor plz!
-apt-get install vim-gnome
+apt-get install vim-gnome --assume-yes
 
 # source control
-apt-get install git-core subversion
+apt-get install git-core subversion --assume-yes
 
 # servers
-apt-get install apache2 mysql-server mysql-query-browser 
+apt-get install apache2 mysql-server mysql-query-browser --assume-yes
 
 # ruby (and deps)
-apt-get install ruby rake rubygems libxslt1-dev irb ri rdoc ruby1.8-dev libzlib-ruby libyaml-ruby libreadline-ruby libncurses-ruby libcurses-ruby libruby libruby-extras libfcgi-ruby1.8 build-essential libopenssl-ruby libdbm-ruby libdbi-ruby libdbd-sqlite3-ruby sqlite3 libsqlite3-dev libsqlite3-ruby libxml-ruby libxml2-dev 
+apt-get install ruby rake rubygems libxslt1-dev irb ri rdoc ruby1.8-dev libzlib-ruby libyaml-ruby libreadline-ruby libncurses-ruby libcurses-ruby libruby libruby-extras libfcgi-ruby1.8 build-essential libopenssl-ruby libdbm-ruby libdbi-ruby libdbd-sqlite3-ruby sqlite3 libsqlite3-dev libsqlite3-ruby libxml-ruby libxml2-dev --assume-yes
 
 # install gems
 gem install cucumber -v=0.6.4
@@ -28,14 +28,11 @@ PATH=$PATH:/var/lib/gems/1.8/bin:/home/dev/.gem/ruby/1.8/bin
 
 #setup passenger
 gem install passenger -v=2.2.11
-apt-get install apache2-prefork-dev libapr1-dev libaprutil1-dev
+apt-get install apache2-prefork-dev libapr1-dev libaprutil1-dev --assume-yes
 passenger-install-apache2-module
 
-PASSENGER_CONF = "LoadModule passenger_module /var/lib/gems/1.8/gems/passenger-2.2.11/ext/apache2/mod_passenger.so
+PASSENGER_CONF="LoadModule passenger_module /var/lib/gems/1.8/gems/passenger-2.2.11/ext/apache2/mod_passenger.so
 PassengerRoot /var/lib/gems/1.8/gems/passenger-2.2.11
 PassengerRuby /usr/bin/ruby1.8
 "
 echo @PASSENGER_CONF > /etc/apache2/conf.d/passenger.conf
-
-#checkout rails-apps
-svn co http://repo.dev.cint.com:8080/svn/rcint/rails_apps ~/code/
