a39506845bbc6f5f32546b5ef29b1086d67c3bea
kitofr <kitofr@gmail.com>
2010-05-05 17:26:57 +0200

    added 'desktop' support

--- ubuntu-10/install.sh
+++ ubuntu-10/install.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-# editor plz!
-apt-get install vim-gnome --assume-yes
-
 # source control
 apt-get install git-core subversion --assume-yes
 
 # servers
 apt-get install apache2 mysql-server mysql-query-browser --assume-yes
 
+# desktop tools
+if [ $# -gt 0 -a $1 = "desktop" ]; then
+  echo "Installing desktop tools"
+  apt-get install vim-gnome --assume-yes
+fi
+
 # ruby (and deps)
 apt-get install ruby rake rubygems libxslt1-dev irb ri rdoc ruby1.8-dev libzlib-ruby libyaml-ruby libreadline-ruby libncurses-ruby libcurses-ruby libruby libruby-extras libfcgi-ruby1.8 build-essential libopenssl-ruby libdbm-ruby libdbi-ruby libdbd-sqlite3-ruby sqlite3 libsqlite3-dev libsqlite3-ruby libxml-ruby libxml2-dev --assume-yes
 
 # install gems
 gem install cucumber -v=0.6.4
-gem install actionmailer actionpack activerecord activeresource activesupport acts_as_audited authlogic builder cgi_multipart_eof_fix cucumber-rails daemons database_cleaner diff-lcs fastthread gem_plugin haml jrails json_pure mocha mongrel mysql open4 polyglot rack rack-test rails rake rspec rspec-rails rubygems-update selenium-client term-ansicolor treetop uuidtools vlad nokogiri fastercsv webrat
+gem install actionmailer actionpack activerecord activeresource activesupport acts_as_audited authlogic builder cgi_multipart_eof_fix cucumber-rails daemons database_cleaner diff-lcs fastthread gem_plugin haml jrails json_pure mocha mongrel mysql open4 polyglot rack rack-test rails rake rspec rspec-rails rubygems-update selenium-client term-ansicolor treetop uuidtools vlad nokogiri fastercsv webrat rest-client simple-rss
 
 gem sources -a http://gems.github.com
 gem install mislav-will_paginate spicycode-rcov
