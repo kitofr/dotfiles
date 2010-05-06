2dc8e0f11d71b36cfa271bf67b5f9afb197d1965
kitofr <kitofr@gmail.com>
2010-05-06 14:55:57 +0200

    Added mysql-lib for ruby

--- ubuntu-10/install.sh
+++ ubuntu-10/install.sh
@@ -24,14 +24,15 @@ if [ $# -gt 0 -a $1 = "development" ]; then
 fi
 
 # ruby (and deps)
-apt-get install ruby rake rubygems libxslt1-dev irb ri rdoc ruby1.8-dev libzlib-ruby libyaml-ruby libreadline-ruby libncurses-ruby libcurses-ruby libruby libruby-extras libfcgi-ruby1.8 build-essential libopenssl-ruby libdbm-ruby libdbi-ruby libdbd-sqlite3-ruby sqlite3 libsqlite3-dev libsqlite3-ruby libxml-ruby libxml2-dev --assume-yes
+apt-get install ruby rake rubygems libdbd-mysql-ruby1.8 libxslt1-dev irb ri rdoc ruby1.8-dev libzlib-ruby libyaml-ruby libreadline-ruby libncurses-ruby libcurses-ruby libruby libruby-extras libfcgi-ruby1.8 build-essential libopenssl-ruby libdbm-ruby libdbi-ruby libdbd-sqlite3-ruby sqlite3 libsqlite3-dev libsqlite3-ruby libxml-ruby libxml2-dev --assume-yes
 
 # install gems
-gem install cucumber -v=0.6.4
-gem install actionmailer actionpack activerecord activeresource activesupport acts_as_audited authlogic builder cgi_multipart_eof_fix cucumber-rails daemons database_cleaner diff-lcs fastthread gem_plugin haml jrails json_pure mocha mongrel mysql open4 polyglot rack rack-test rails rake rspec rspec-rails rubygems-update selenium-client term-ansicolor treetop uuidtools vlad nokogiri fastercsv webrat rest-client simple-rss
+NO_RDOC="--no-ri --no-rdoc"
+gem install cucumber -v=0.6.4 $NO_RDOC
+gem install actionmailer actionpack activerecord activeresource activesupport acts_as_audited authlogic builder cgi_multipart_eof_fix cucumber-rails daemons database_cleaner diff-lcs fastthread gem_plugin haml jrails json_pure mocha mongrel mysql open4 polyglot rack rack-test rails rake rspec rspec-rails rubygems-update selenium-client term-ansicolor treetop uuidtools vlad nokogiri fastercsv webrat rest-client simple-rss $NO_RDOC
 
 gem sources -a http://gems.github.com
-gem install mislav-will_paginate spicycode-rcov
+gem install mislav-will_paginate spicycode-rcov $NO_RDOC
 
 # trollop - extra gem... 
 # haml upgrade 2.2.23 -> 2.2.24
@@ -41,7 +42,7 @@ gem install mislav-will_paginate spicycode-rcov
 PATH=$PATH:/var/lib/gems/1.8/bin:/home/dev/.gem/ruby/1.8/bin
 
 #setup passenger
-gem install passenger -v=2.2.11
+gem install passenger -v=2.2.11 $NO_RDOC
 apt-get install apache2-prefork-dev libapr1-dev libaprutil1-dev --assume-yes
 passenger-install-apache2-module
 
