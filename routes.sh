a7bda8e2ebf42a7e8e1f9e8d6972601190ea2b3a
kitofr <kitofr@gmail.com>
2010-06-10 11:07:59 +0200

    Removed some routes

--- routes.sh
+++ routes.sh
@@ -3,7 +3,7 @@
 route delete default
 route add default gw gw2.stockholm.cint.com
 route add -net 10.23.96.0 netmask 255.255.248.0 gw 192.168.121.3
-route add -net 10.23.12.0 netmask 255.255.255.0 gw 192.168.121.4
-route add -net 10.23.11.0 netmask 255.255.255.0 gw 192.168.121.4
-route add -net 10.23.10.0 netmask 255.255.255.0 gw 192.168.121.4
+#route add -net 10.23.12.0 netmask 255.255.255.0 gw 192.168.121.4
+#route add -net 10.23.11.0 netmask 255.255.255.0 gw 192.168.121.4
+#route add -net 10.23.10.0 netmask 255.255.255.0 gw 192.168.121.4
 
