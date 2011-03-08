04a8a9a117aa28b6a9252d40743bff8c4339791c
Kristoffer Roupé <kitofr@gmail.com>
2011-03-08 18:03:46 +0000

    Updated some, included html5.vim

--- ubuntu-10/install.sh
+++ ubuntu-10/install.sh
@@ -12,3 +12,6 @@ apt-get install vim-gnome xvfb mysql-server mysql-query-browser --assume-yes
 
 # ruby (and deps)
 apt-get install ruby1.8-dev ruby1.8 rake rails rubygems libdbd-mysql-ruby1.8 libxslt1-dev irb ri rdoc libzlib-ruby libyaml-ruby libreadline-ruby libncurses-ruby libcurses-ruby libruby libruby-extras libfcgi-dev libfcgi-ruby1.8 build-essential libopenssl-ruby libdbm-ruby libdbi-ruby libdbd-sqlite3-ruby sqlite3 libsqlite3-dev librestclient-ruby1.8 libsqlite3-ruby libxml-ruby libxml2-dev libffi-ruby1.8 --assume-yes
+
+# gems
+gem install bundler rvm
