598d6cabb36416222db9ee1297003007b91b6201
kitofr <kitofr@gmail.com>
2011-03-18 16:31:03 +0100

    Added in ir_black vim scheme and a regedit script to set things up on windows

--- windows/startup.cmd
+++ windows/startup.cmd
@@ -1,10 +1,15 @@
 @echo off
+doskey history=doskey /history
 doskey ..=cd ..
 doskey ls=dir
 doskey l=dir
+doskey ll=dir
 doskey pwd=cd
 doskey dotfiles=cd c:\Work\kitofr\dotfiles
 doskey trunk=cd c:\Work\Cint\trunk
 doskey adapter=cd c:\Work\Cint\trunk\src\Web.Cpx.Adapter\
+doskey sampling=cd c:\Work\Cint\trunk\src\Cint.Sampling\
 doskey home=cd c:\Users\Kristoffer
-
+doskey kitofr=cd c:\Work\kitofr\
+doskey spikes=cd c:\Work\Cint\spikes
+doskey rm=del
