667cde67c0202abc59741a32a4abb8bf269ec2af
kitofr <kitofr@gmail.com>
2012-02-04 14:29:30 +0100

    Added curl and vimsetup

--- vim/install.bat
+++ vim/install.bat
@@ -8,6 +8,8 @@ set VIMRC_SOURCE=%~dp0vimrc
 set VIMRC_TARGET=%HOMEDRIVE%%HOMEPATH%\_vimrc
 set VIMFILES_SOURCE=%~dp0
 set VIMFILES_TARGET=%HOMEDRIVE%%HOMEPATH%\.vim
+set CURL_SOURCE=%~dp0curl.cmd
+set CURL_TARGET=C:\Program Files\Git\cmd\curl.cmd
 
 if exist %VIMRC_TARGET% (
 	echo warning: '%VIMRC_TARGET%' file already exists, please delete manually and try again.
@@ -19,4 +21,10 @@ if exist %VIMFILES_TARGET% (
 	echo warning: '%VIMFILES_TARGET%' directory exists, please delete manually and try again.
 ) else (
 	mklink /J "%VIMFILES_TARGET%" "%VIMFILES_SOURCE%"
+)
+
+if exist %CURL_TARGET% (
+	echo warning: '%CURL_TARGET%' directory exists, please delete manually and try again.
+) else (
+	mklink "%CURL_TARGET%" "%CURL_SOURCE%"
 )
