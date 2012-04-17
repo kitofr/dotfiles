66fe177e32034176ebc09fff0475c22b4725f907
Kristoffer Roupe <kristoffer.roupe@cint.com>
2012-04-17 08:50:47 +0200

    Removed powershell stuff

--- windows/install.ps1
+++
@@ -1,8 +0,0 @@
-$cmd = "mklink /J ""$($profile.AllUsersCurrentHost)"" $($pwd)profile.ps1"
-& cmd /c $cmd
-
-Set-Alias ngen @(
-    dir (join-path ${env:\windir} "Microsoft.NET\Framework") ngen.exe -recurse |
-    sort -descending lastwritetime
-    )[0].fullName
-[appdomain]::currentdomain.getassemblies() | %{ngen $_.location}
