a8dd5e066113687df6d81694e04d935c05d132bb
Kristoffer Roupe <kitofr@gmail.com>
2014-01-14 21:48:09 +0100

    chocolatey inside

---
+++ chocolatey/packages.ps1
@@ -0,0 +1,17 @@
+# development
+cinst msysgit -force
+cinst ruby -force
+cinst nodejs.install -force
+cinst lein -force
+cinst Elixir -force
+cinst DotNet4.5 -force
+
+# dev tools
+cinst dotPeek -force
+cinst linqpad -force
+cinst EthanBrown.ConEmuConfig -force
+
+# tools
+cinst GoogleChrome -force
+cinst 7zip -force
+cinst sysinternals -force
