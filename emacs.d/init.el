586c6e911c5b6092ec1b9ee59b0afca47658b8c4
Kristoffer Roupé <kitofr@gmail.com>
2015-05-15 21:35:23 +0200

    Emacs etc

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,8 +1,8 @@
 (require 'package)
 
 (setq package-archives
-        '(("gnu"         . "http://elpa.gnu.org/packages/")
-              ("melpa"       . "http://melpa.milkbox.net/packages/")))
+      '(("gnu"  . "http://elpa.gnu.org/packages/")
+	("melpa". "http://melpa.milkbox.net/packages/")))
 
 (package-initialize)
 
